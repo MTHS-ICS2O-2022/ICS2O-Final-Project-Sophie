ec7a2a04ce3d84086b924677c2005d1bb7e6bc30
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-01-18 01:18:00 +0000

    js

--- js/script.js
+++ js/script.js
@@ -13,6 +13,25 @@ if (navigator.serviceWorker) {
   })
 }
 
-function startClicked() {
+function submitClicked() {
+
+  const answer = String(document.getElementById('answer').value)
+  const pics = [
+    url ('../images/whobulbasaur.jpg'),
+    url ('../images/whodiglet.jpg'),
+    url ('../images/whoevee.png'),
+    url ('../images/whojigglypuff.jpg'),
+    url ('../images/wholapras.jpg'),
+    url ('../images/whomewtwo.jpg'),
+    url ('../images/whomimikyu.jpg'),
+    url ('../images/whopsyduck.png'),
+    url ('../images/whovaporeon.jpg'),
+    url ('../images/whomudkip.png'),
+  ]
+  
+  sessionstorage.count++
+
+
+  
   
 }
