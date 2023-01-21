35c1e0f20ae17797985d33cf22f7690e4060d945
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-01-21 14:33:19 +0000

    js

--- js/script.js
+++ js/script.js
@@ -75,28 +75,44 @@ if (navigator.serviceWorker) {
   let id = 0
   let points = 0
 
+function checkClicked() {
+  const answer = String(document.getElementById('answer').value)
+  
+  if (id<10) {
+    document.getElementById('answers-img').innerHTML = '<h5>'+answer+'</h5>'
+    if (answer == question[id].answer) {
+      document.getElementById('right-wrong').innerHTML = 'correct!'
+      
+    }
+    else {
+      document.getElementById('right-wrong').innerHTML = 'wrong! correct answer was ' + question[id].answer + '!'
+    }
+  }
+}
+
 function submitClicked() {
 
   const answer = String(document.getElementById('answer').value)
+  const answerEvaluated = String(document.getElementById('answers-img').value)
   
   start = false
 
   if (id<10) {
-    if (answer == question[id].answer) {
+    if (answer == question[id].answer || answerEvaluated == question[id].answer) {
       points++
     }
     id++
-    document.getElementById('question').innerHTML = '<img src="' + question[id].images + '" alt="pokemon" height="50%" width="50%"/>'
+    document.getElementById('question-result').innerHTML = '<img src="' + question[id].images + '" alt="pokemon" height="50%" width="50%"/>'
   }
   else {
-    document.getElementById('all').innerHTML = '<h4> you got: </h4> <h4>' + points + '/11 </h4>'
+    document.getElementById('question-result').innerHTML = '<h4> you got: </h4> <h4>' + points + '/11 </h4>'
     if (points < 6) {
-      document.getElementById('endimg').innerHTML = '<img src="./images/pikachusleeppokeball.gif " alt="pokemon" height="50%" width="50%"/> <h5> want to retry?</h5>'
+      document.getElementById('answers-img').innerHTML = '<img src="./images/pikachusleeppokeball.gif " alt="pokemon" height="50%" width="50%"/> <h5> want to retry?</h5>'
     }
     else {
-      document.getElementById('endimg').innerHTML = '<img src="./images/pikachupokeball.gif" alt="pokemon" height="50%" width="50%"/> <h5> want to retry?</h5>'
+      document.getElementById('answers-img').innerHTML = '<img src="./images/pikachupokeball.gif" alt="pokemon" height="50%" width="50%"/> <h5> want to retry?</h5>'
     }
-    document.getElementById('returnbutton').innerHTML = '<a href="./index.html"> <buttonclass="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"> retry</button></a>'
+    document.getElementById('button').innerHTML = '<a href="./index.html"> <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"> retry</button></a>'
     start = true
   }
 
@@ -108,3 +124,4 @@ function submitClicked() {
     points = 0
   }
 }
+
