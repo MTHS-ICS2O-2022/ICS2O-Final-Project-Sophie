7a0c0d393a16105c382b34b769072789cd43ab81
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-01-21 00:54:33 +0000

    js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ if (navigator.serviceWorker) {
   })
 }
 
-  let stage = [
+  let question = [
     {
       id:0,
       images:'./images/whopikachu.jpg',
@@ -81,18 +81,27 @@ function submitClicked() {
   
   start = false
 
-  if (id<11) {
-    if (answer == stage[id].answer) {
+  if (id<10) {
+    if (answer == question[id].answer) {
       points++
     }
-    document.getElementById('question').innerHTML = '<img src="' + stage[id].images + '" alt="pokemon" height="50%" width="50%"/>'
     id++
+    document.getElementById('question').innerHTML = '<img src="' + question[id].images + '" alt="pokemon" height="50%" width="50%"/>'
   }
   else {
     document.getElementById('all').innerHTML = '<h4> you got: </h4> <h4>' + points + '/11 </h4>'
+    if (points < 6) {
+      document.getElementById('endimg').innerHTML = '<img src="./images/pikachusleeppokeball.gif " alt="pokemon" height="50%" width="50%"/> <h5> want to retry?</h5>'
+    }
+    else {
+      document.getElementById('endimg').innerHTML = '<img src="./images/pikachupokeball.gif" alt="pokemon" height="50%" width="50%"/> <h5> want to retry?</h5>'
+    }
+    document.getElementById('returnbutton').innerHTML = '<a href="./index.html"> <buttonclass="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"> retry</button></a>'
+    start = true
   }
 
   console.log(id)
+  console.log(points)
 
   if (start == true) {
     id = 0
